485fbfa41d32b274e1927126630370b9fdec9a0f
Lanser11 <98963962+Lanser11@users.noreply.github.com>
2022-02-23 19:59:09 +0530

    e mail program

---
+++ sendingemail.py
@@ -0,0 +1,26 @@
+#used to create a smtp object
+
+import smtplib
+
+smtpObj = smtplib.SMTP( [host [, port [, local_hostname]]] )
+
+#program
+
+import smtplib
+
+sender = 'from@fromdomain.com'
+receivers = ['to@todomain.com']
+
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)         
+   print ("Successfully sent email")
+except SMTPException:
+   print ("Error: unable to send email")
